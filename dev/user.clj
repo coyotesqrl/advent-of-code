7c2b03918d49af17064f2b5e5b9a347d5b988008
R.A. Porter <coyotesqrl@gmail.com>
2022-12-08 06:34:59 -0700

    2022 Day 8 - also adds some minimal template gen

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,8 @@
 (ns user
-  (:require [nextjournal.clerk :as clerk]
-            [portal.api :as p]))
+  (:require [clojure.java.io :as io]
+            [nextjournal.clerk :as clerk]
+            [portal.api :as p]
+            [selmer.parser :as sp]))
 
 (defn portal []
   (add-tap #'portal.api/submit)
@@ -12,3 +14,11 @@
 
 (defn clerk-show [y d]
   (clerk/show! (format "src/coyotesqrl/%d/day%02d.clj" y d)))
+
+(defn init-day [y d]
+  (let [out-ns (format "src/coyotesqrl/%d/day%02d.clj" y d)
+        #_#_input  (format "resources/coyotesqrl/%d/day%d-input.txt" y d)]
+    (spit out-ns (-> "coyotesqrl/clj-template.txt"
+                     (io/resource)
+                     (sp/render-file {:year y :day d})))
+    #_(spit input (slurp (format "https://adventofcode.com/%d/day/%d/input" y d)))))
